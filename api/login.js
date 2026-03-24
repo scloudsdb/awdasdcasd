20940561bda19976c324bd02523206d8055f7a8b
scloudsdb <steamcloudsdta@gmail.com>
2026-03-24 11:07:41 +0700

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,8 +1,12 @@
 export default function handler(req, res) {
+  const redirect = process.env.DISCORD_REDIRECT_URI;
+
+  console.log("REDIRECT:", redirect);
+
   const params = new URLSearchParams({
     client_id: process.env.DISCORD_CLIENT_ID,
     response_type: "code",
-    redirect_uri: process.env.DISCORD_REDIRECT_URI,
+    redirect_uri: redirect,
     scope: "identify guilds",
   });
 
